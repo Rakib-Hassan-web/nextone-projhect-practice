07328f8e1b68fffc554efe7a8c0f4ab8f7955ed1
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-05 16:10:57 +0600

    complete

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,46 +9,51 @@ import { RiShoppingCartLine } from "react-icons/ri";
 
 
 const Navbar = () => {
-  return (
-    <>
-    
-    
+return (
+<>
+
+
 
     <div className='container py-[27px]'>
 
 
-    <div className='nav_row flex justify-between items-center'>
+        <div className='nav_row flex justify-between items-center'>
 
-<Link className='nav_logo' ><img src={navlogo} alt=""  className='w-[119px]'/></Link>
-
-
-
-<div className="navsrach w-[400px] h-[52px] bg-[#F8F8F8] rounded-[100px] flex items-center gap-[10px] pl-[24px]">
-
-<RiSearch2Line className='text-2xl text-prymary'/>
-
-<input type="text"  className='w-full text-[16px] outline-none border-none text-prymary '  placeholder='Search in products... '/>
-
-</div>
+            <Link className='nav_logo'><img src={navlogo} alt="" className='w-[119px]' /></Link>
 
 
-<div className="nav_icon flex items-center gap-[10px]">
 
-<RiUserLine className='text-2xl'/>
-<RiShoppingCartLine className='text-2xl'/>
+            <div
+                className="navsrach w-[400px] h-[52px] bg-[#F8F8F8] rounded-[100px] flex items-center gap-[10px] pl-[24px]">
 
-</div>
+                <RiSearch2Line className='text-2xl text-prymary' />
+
+                <input type="text" className='w-full text-[16px] outline-none border-none text-prymary '
+                    placeholder='Search in products... ' />
+
+            </div>
+
+
+            <div className="nav_icon flex items-center  w-[78px]">
+
+                <RiUserLine className='text-2xl mr-[10px]' />
+                <RiShoppingCartLine className='text-2xl relative' />
+
+                <div
+                    className='  right-[204px] top-[32px] w-[20px] h-[20px] bg-[#0EA5E9] rounded-full text-[14px] text-white flex justify-center items-center absolute'>
+                    3</div>
+            </div>
+
+
+        </div>
+
 
 
     </div>
 
 
-
-    </div>
-    
-    
-    </>
-  )
+</>
+)
 }
 
 export default Navbar
