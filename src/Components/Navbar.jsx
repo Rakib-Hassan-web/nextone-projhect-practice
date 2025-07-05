4bd9d107b05cb44d2102d5fde75f9a6b69bba57e
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-05 15:53:29 +0600

    search icon set

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router'
 
 import navlogo from '../assets/image/logo.png'
+import { RiSearch2Line } from "react-icons/ri";
+
 
 const Navbar = () => {
   return (
@@ -12,13 +14,19 @@ const Navbar = () => {
     <div className='container py-[27px]'>
 
 
-    <div className='nav_row'>
+    <div className='nav_row flex justify-between items-center'>
 
 <Link className='nav_logo' ><img src={navlogo} alt=""  className='w-[119px]'/></Link>
 
 
 
-<div className="navsrach"></div>
+<div className="navsrach w-[400px] h-[52px] bg-[#F8F8F8] rounded-[100px] flex items-center gap-[10px] pl-[24px]">
+
+<RiSearch2Line className='text-2xl text-prymary'/>
+
+<input type="text"  c/>
+
+</div>
 
 
 <div className="nav_icon"></div>
