eb9b48cf3c218c277497720ab0663cf1e06b2da7
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-05 15:40:21 +0600

    all ready need to make navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router'
+import LayoutOne from './layout/LayoutOne'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const myroute =createBrowserRouter(createRoutesFromElements(
+
+
+    <Route>
+
+<Route path='/' element={<LayoutOne/>}>
+
+<Route index element={<Home/>}/>
+
+
+
+</Route>
+
+
+
+
+
+    </Route>
+
+
+
+  ))
   return (
 
-    const myroute =
     <>
       
+
+      <RouterProvider router={myroute}/>
     </>
   )
 }
--- src/layout/LayoutOne.jsx
+++ src/layout/LayoutOne.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Outlet } from 'react-router'
+import Navbar from '../Components/Navbar'
 
 const LayoutOne = () => {
   return (
@@ -8,7 +10,9 @@ const LayoutOne = () => {
     <>
     
     
- 
+
+    <Navbar/>
+ <Outlet/>
     
     
     </>
