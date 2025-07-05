86d961b1df2eac1f6e2eec48edb299a7a499dcca
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-05 15:43:16 +0600

    basic structure

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Navbar = () => {
   return (
     <>
     
     
+
+    <div className='container'>
+
+
+    <div className='nav_row'>
+
+<Link className='nav_logo' ><img src="" alt="" /></Link>
+
+
+
+<div className="navsrach"></div>
+
+
+<div className="nav_icon"></div>
+
+
+    </div>
+
+
+
+    </div>
     
     
     </>
