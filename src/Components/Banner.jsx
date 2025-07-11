5366f2c0520ff72efe0652b883fdaf96566192b9
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-11 11:52:25 +0600

    banner containrer link  tag add

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 import bannerbg from '../assets/image/bannerimg.png'
+import { Link } from 'react-router'
+import { CiSearch } from 'react-icons/ci'
 
 const Banner = () => {
   return (
@@ -14,6 +16,11 @@ const Banner = () => {
         <h2>Starting from: $49.99</h2>
         <h2>Exclusive collection
              for everyone</h2>
+             <Link to={'#'}
+                    className='bg-black flex lg:gap-3 gap-2 rounded-full items-center text-white text-[14px] lg:text-[16px] font-medium font-praymary py-[12px] lg:py-[20px] lg:pl-[36px] pl-[24px] w-fit   lg:pr-[50px] pr-[19px] hover:scale-[1.1] duration-[.4s] '>
+                Explore now
+                <CiSearch className='lg:text-3xl text-2xl  ' />
+                </Link>
     </div>
 
 </section>
